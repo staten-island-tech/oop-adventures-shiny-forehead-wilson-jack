27956dbd5ac0c8b33afea01b5427504db8fa3886
1234-000 <wilsonm68@nycstudents.net>
2026-05-07 09:08:42 -0400

    k

--- BlackJack.py
+++ BlackJack.py
@@ -1,29 +1,39 @@
 import requests
 import tkinter as tk
-from tkinter import messagebox
 import random
 
-
+#url = https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1
 
 '''class BlackJack:
-    def __init__(self,player,dealer,decks,shuffle):
+    def __init__(self, value, suit):
+        self.value = value
+        self. suit = suit'''
+
+        
 
 
 
-    def cards():
-        url = https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'''
 
 
-
-coins = 10 
+#coins = 10 
 
 '''while True: ##### We are gonna use a while statement. If user coins = 0. Break'''
 
 
 window = tk.Tk()
-window.title("BlackJack Degen")
-window.geometry("400x250")
-window.resizable(False, False)
+window.title("Blackjack")
+window.geometry("400x300")
+window.resizable(False,False)
 
-header = tk.Label(window, text = "BLACKJACK")
+header = tk.Label(window, text = "BLACKJACK", font = 100)
 header.pack(pady = 10)
+
+
+btn = tk.Button(window, text = "Stand", font = 50)
+btn.pack(side="bottom",padx=50)
+btn = tk.Button(window, text="Hit", font = 50)
+btn.pack(side="bottom",padx=50)
+btn.pack()
+window.mainloop()
+
+
