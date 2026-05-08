62153106fedb9035cc2065f0b22371b1ae678c76
1234-000 <wilsonm68@nycstudents.net>
2026-05-08 09:10:57 -0400

    mn

---
+++ Desert.py
@@ -0,0 +1,57 @@
+import random
+
+class Desert:
+    def __init__(self, name, happy, hunger, iventory, thrist, energy, mood, health):
+        self.name = name
+        self.__happy = happy
+        self.__hunger = hunger
+        self.__inventory = iventory
+        self.__thrist = thrist
+        self.__energy = energy
+        self.__mood = mood
+        self.__health = health
+    
+    def newnumber():
+        return random.randint(20,50)
+
+    def water(self):
+        self.__thrist + self.newnumber()
+        self.__energy + self.newnumber()
+        self.__happy + self.newnumber()
+        self.__health + self.newnumber()
+
+    def feeling(self):
+        if self.__thrist and self.__energy and self.__happy and self.__health < 25:
+            print("Near Death")
+        elif self.__thrist and self.__energy and self.__happy and self.__health < 50:
+            print("Depressed")
+        elif self.__thrist and self.__energy and self.__happy and self.__health > 50:
+            print("Alright")
+
+
+
+
+
+
+#coins = 10 
+
+'''while True: ##### We are gonna use a while statement. If user coins = 0. Break'''
+
+
+'''window = tk.Tk()
+window.title("Blackjack")
+window.geometry("400x300")
+window.resizable(False,False)
+
+header = tk.Label(window, text = "BLACKJACK", font = 100)
+header.pack(pady = 10)
+
+
+btn = tk.Button(window, text = "Stand", font = 50)
+btn.pack(side="bottom",padx=50)
+btn = tk.Button(window, text="Hit", font = 50)
+btn.pack(side="bottom",padx=50)
+btn.pack()
+window.mainloop()'''
+
+
