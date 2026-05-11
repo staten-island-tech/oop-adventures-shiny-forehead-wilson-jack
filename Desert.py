c0bee54fab00380054928eee227669bf289916d0
1234-000 <wilsonm68@nycstudents.net>
2026-05-11 09:07:19 -0400

    a

--- Desert.py
+++ Desert.py
@@ -27,40 +27,31 @@ class Desert:
             print("Depressed")
         elif self.__thrist > 50 and self.__energy > 50 and self.__happy > 50 and self.__health > 50:
             print("Alright")
+    def fishing(self):
+        asker = input("Fish?").lower()
+        if asker == "yes":
+            self.__energy += self.newnumber
+            self.__happy += self.newnumber
+            self.__health += self.newnumber
+            self.__hunger += self.newnumber
 
 
 
 store = [
-    "Sword": {"name": "Sword", "description": "Heavy and deals damage"},
-    "Water": {"name": "Water Bottle", "description": "Refreshing"},
-
+"Sword": {"name": "Sword", "description": "Heavy and deals damage"},
+"Water": {"name": "Water Bottle", "description": "Refreshing"}, 
+"Shovel": {"name": "Shovel", "description": "Dig for treasure"}, 
+"WW2 Rations": {"name": "WW2 Rations", "description": "Food for energy"},
+"Energy Drnk": {"name": "Energy Drink", "description": "Drink for Energy"},
+"Fishing Rod": {"name": "Fishing Rod", "description": "Fish for Food"}
 ]
-print[store]["name"]
 
 
 
 
 
 
-#coins = 10 
-
-'''while True: ##### We are gonna use a while statement. If user coins = 0. Break'''
 
 
-'''window = tk.Tk()
-window.title("Blackjack")
-window.geometry("400x300")
-window.resizable(False,False)
-
-header = tk.Label(window, text = "BLACKJACK", font = 100)
-header.pack(pady = 10)
-
-
-btn = tk.Button(window, text = "Stand", font = 50)
-btn.pack(side="bottom",padx=50)
-btn = tk.Button(window, text="Hit", font = 50)
-btn.pack(side="bottom",padx=50)
-btn.pack()
-window.mainloop()'''
 
 
--- project.py
+++
@@ -1,2 +0,0 @@
-class Blackjack:
-    def __init__(self,  ):
