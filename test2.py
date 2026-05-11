7436f9c154c0719e0a48a97de63effb51f869650
jakcL142 <jackl142@nycstudents.net>
2026-05-11 09:22:15 -0400

    h

--- test2.py
+++ test2.py
@@ -7,7 +7,7 @@ def heavy_storm(self):
     self.__health -= 10
     print("A heavy storm hits!")
 def find_food(self):
-    self.__hunger -= 10
+    self.__hunger -= 15
     self.__happy += 5
     print("You found some food! It's not much, but, you're content with it.")
 def get_sick(self):
@@ -17,8 +17,18 @@ def get_sick(self):
 def perfect_weather(self):
     self.__happy += 15
     print("Perfect weather today. Somehow, you feel eventful already.")
+def wild_beast(self):
+    print()
+    print("A wild boar charges out from the forest!")
+    print("1. Hide(costs energy, but you're safe)")
+    print("2. Confront it (Risks injury, but might gain happiness)")
+    choice = input("What do you do? (1/2):")
+    if choice == "1":
+            self.__energy -= 15
+
+
 
 def random_event(self):
-    events = [self.storm, self.heavy_storm, self.find_food, self,get_sick, self.perfect_weather]
+    events = [self.storm, self.heavy_storm, self.find_food, self.get_sick, self.perfect_weather]
     event = random.choice(events)
     event()
