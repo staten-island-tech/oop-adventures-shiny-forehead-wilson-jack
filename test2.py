1991825b1f807fbd97bf618b3e1707e6a0549339
1234-000 <wilsonm68@nycstudents.net>
2026-05-13 09:25:08 -0400

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,9 +1,11 @@
-import random
-
 def storm(self):
     self.__energy -= 10
     self.__health -= 5
     print("A storm hits!")
+def heavy_storm(self):
+    self.__energy -= 20
+    self.__health -= 10
+    print("A heavy storm hits!")
 def find_food(self):
     self.__hunger -= 15
     self.__happy += 5
@@ -13,21 +15,10 @@ def get_sick(self):
     self.__mood -= 15
     print("Your immune system was compromised. Yes, you got sick...")
 def perfect_weather(self):
-    self.__happy += 10
+    self.__happy += 15
     print("Perfect weather today. Somehow, you feel hopeful already.")
-def volcanic_eruption(self):
-        self.__health -= 30
-        self.__energy -= 20
-        self.__happy -= 20
-        print("A volcano erupts! The air is thick with suffocating smoke as you rush towards safety.")
 
-def supply_crate(self):
-     print()
-     print("1. Bandages")
-     print("2. Can of beans")
-     print("3. Bottle of water")
-     print("4. Bag of coins")
-     print("5. Spear")
+          
 
 
 def wild_beast(self):
