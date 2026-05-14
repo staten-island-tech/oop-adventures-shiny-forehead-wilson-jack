a619352ecba0ddd45ddb0d48222c835845b5076b
jakcL142 <jackl142@nycstudents.net>
2026-05-14 09:22:15 -0400

    h

--- test2.py
+++ test2.py
@@ -1,24 +1,28 @@
 import random
 
+random.randint
+
 def storm(self):
     self.__energy -= 10
     self.__health -= 5
     print("A storm hits!")
 def find_food(self):
+    self.__health += 15
     self.__hunger -= 15
-    self.__happy += 5
-    print("You found some food! It's not much, but, you're content with it.")
+    self.__happy += 10
+    print("You found some food! It wasn't much, but you gobble it up anyway.")
 def get_sick(self):
-    self.__health -= 15
+    self.__health -= 10
     self.__mood -= 15
+    self.__happy -= 15
     print("Your immune system was compromised. Yes, you got sick...")
 def perfect_weather(self):
     self.__happy += 10
     print("Perfect weather today. Somehow, you feel hopeful already.")
 def volcanic_eruption(self):
-        self.__health -= 30
-        self.__energy -= 20
-        self.__happy -= 20
+        self.__health -= 25
+        self.__energy -= 25
+        self.__happy -= 15
         print("A volcano erupts! The air is thick with suffocating smoke as you rush towards safety.")
 
 def supply_crate(self):
@@ -28,6 +32,8 @@ def supply_crate(self):
      print("3. Bottle of water")
      print("4. Bag of coins")
      print("5. Spear")
+     choice = input("Which items will you take(Max 2)?: ")
+     #  COME BACK AGAIN WHEN INVENTORY IS ADDED
 
 
 def wild_beast(self):
@@ -40,14 +46,18 @@ def wild_beast(self):
             self.__energy -= 15
             print("You dove into the thick brush. You're exhausted, but safe.")
     elif choice == "2":
-         if random.choice([True, False]): self.__happy += 20
-         print("You scared it off! You feel like a survivor.")
+         if random.choice([True, False]): 
+              self.__happy += 20
+              print("You scared it off! You feel like a survivor.")
+         else:
+             self.__health -= 20
+             self.__happy -= 10
+             print("The boar tackled you!")
     else:
-         self.__health -= 20
-         self.__happy -= 10
-         print("The boar tackled you! Ouch...")
+        self.__happy -= 1
+        print("The boar wasn't hungry, and lost interest in you. Phew.")
 
 def random_event(self):
-    events = [self.storm, self.heavy_storm, self.find_food, self.get_sick, self.perfect_weather]
+    events = [self.storm, self.find_food, self.get_sick, self.perfect_weather, self.volcanic_eruption, self.supply_crate, self.wild_beast]
     event = random.choice(events)
     event()
