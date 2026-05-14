1a59b4d43511cd674e837cc536c11028e55d5e5d
1234-000 <wilsonm68@nycstudents.net>
2026-05-14 09:21:32 -0400

    s

--- Desert.py
+++ Desert.py
@@ -1,27 +1,36 @@
 import random
 
 class Starter:
-    def __init__(self, name, health):
+    def __init__(self, name, health, coins):
         self.name = name
         self.health = health
+        self.coins = coins
     
 
 class NPC(Starter):
-    def __init__(self,name,health):
-        super().__init__(name, health)
+    def __init__(self,name,health, coins):
+        super().__init__(name, health, coins)
     
 
 
 
 class Deserted(Starter):
-    def __init__(self, name, happy, hunger, inventory, thrist, energy, mood, health, store):
-        super().__init__(name, health)
+    def __init__(self, name, happy, hunger, inventory, thrist, energy, mood, health, coins):
+        super().__init__(name, health, coins)
         self.__happy = happy
         self.__hunger = hunger
         self.__inventory = []
         self.__thrist = thrist
         self.__energy = energy
         self.__mood = mood
+        self.__coins = coins
+    
+    def rich(self):
+        richer = input("Do You Want To Shovel For Coins?").lower()
+        if richer == "yes":
+            self.__coins += random.randint(1,20)
+
+
     
     def newnumber(self):
         return random.randint(20,50)
@@ -87,17 +96,17 @@ class Deserted(Starter):
             self.__happy -= 10
             print("The boar tackled you! Ouch...")
     def market(self ):
-        consumer = input("Do You Want To Buy From Store??? ").lower
+        consumer = input("Do You Want To Buy From Store??? ").lower()
         if consumer == "yes":
             print(store)
         store = {
-        "Sword": {"name": "Sword", "description": "Heavy and deals damage", "Coins": 10},
-        "Water": {"name": "Water Bottle", "description": "Refreshing", "Coins": 5}, 
-        "Shovel": {"name": "Shovel", "description": "Dig for treasure","Coins": 0}, 
-        "WW2 Rations": {"name": "WW2 Rations", "description": "Food for energy", "Coins": 5},
-        "Energy Drnk": {"name": "Energy Drink", "description": "Drink for Energy", "Coins": 3},
-        "Fishing Rod": {"name": "Fishing Rod", "description": "Fish for Food", "Coins": 6},
-        "Raft": {"name": "Life Raft", "description": "Escape Boat To FREEDOM", "Coins": 1000}
+        1 : {"name": "Sword", "description": "Heavy and deals damage", "Coins": 10},
+        2 : {"name": "Water Bottle", "description": "Refreshing", "Coins": 5}, 
+        3 : {"name": "Shovel", "description": "Dig for treasure","Coins": 0}, 
+        4 : {"name": "WW2 Rations", "description": "Food for energy", "Coins": 5},
+        5 : {"name": "Energy Drink", "description": "Drink for Energy", "Coins": 3},
+        6 : {"name": "Fishing Rod", "description": "Fish for Food", "Coins": 6},
+        7 : {"name": "Life Raft", "description": "Escape Boat To FREEDOM", "Coins": 1000}
         }
     def days(self):
         self.energy -= 10
--- test.py
+++ test.py
@@ -1,14 +1,22 @@
 
+Coins = 30
+
+
 store = {
         1 : {"name": "Sword", "description": "Heavy and deals damage", "Coins": 10},
         2: {"name": "Water Bottle", "description": "Refreshing", "Coins": 5}, 
-        "Shovel": {"name": "Shovel", "description": "Dig for treasure","Coins": 0}, 
-        "WW2 Rations": {"name": "WW2 Rations", "description": "Food for energy", "Coins": 5},
-        "Energy Drnk": {"name": "Energy Drink", "description": "Drink for Energy", "Coins": 3},
-        "Fishing Rod": {"name": "Fishing Rod", "description": "Fish for Food", "Coins": 6},
-        "Raft": {"name": "Life Raft", "description": "Escape Boat To FREEDOM", "Coins": 1000}
+        3 : {"name": "Shovel", "description": "Dig for treasure","Coins": 0}, 
+        4 : {"name": "WW2 Rations", "description": "Food for energy", "Coins": 5},
+        5 : {"name": "Energy Drink", "description": "Drink for Energy", "Coins": 3},
+        6 : {"name": "Fishing Rod", "description": "Fish for Food", "Coins": 6},
+        7 : {"name": "Life Raft", "description": "Escape Boat To FREEDOM", "Coins": 1000}
         }
 
-consumer = input("Do You Want To Buy From Store??? ").lower
+consumer = input("Do You Want To Buy From Store???").lower()
 if consumer == "yes":
-    print("work")
+        for i in range(1,7):
+                print(store[i]["name"])
+        buy = input("Buy WHat?")
+        for x in range(1,7):
+                store[x][buy]
+
