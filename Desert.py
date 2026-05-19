2204e1170e2238cb02b74895129db1309cc96358
1234-000 <wilsonm68@nycstudents.net>
2026-05-19 09:23:11 -0400

    a

--- Desert.py
+++ Desert.py
@@ -104,20 +104,39 @@ class Deserted(Starter):
             self.__happy -= 10
             print("The boar tackled you! Ouch...")
     def market(self ):
-        store = {
-        1 : {"name": "Sword-", "description": "Heavy and deals damage", "Coins": 10},
-        2: {"name": "Water Bottle-", "description": "Refreshing", "Coins": 5}, 
-        3 : {"name": "Shovel-", "description": "Dig for treasure","Coins": 0}, 
-        4 : {"name": "WW2 Rations-", "description": "Food for energy", "Coins": 5},
-        5 : {"name": "Energy Drink-", "description": "Drink for Energy", "Coins": 3},
-        6 : {"name": "Fishing Rod-", "description": "Fish for Food", "Coins": 6},
-        7 : {"name": "Life Raft-", "description": "Escape Boat To FREEDOM", "Coins": 1000}
-        }
+        cart = []
 
-        consumer = input("Do You Want To Buy From Store???").lower()
-        if consumer == "yes":
-            for i in range(1,8): 
-                print(store[i]["name"], "Description:", store[i]["description"])
+    store = [
+         {"name": "Sword", "description": "Heavy and deals damage", "Coins": int(10)},
+         {"name": "Water Bottle", "description": "Refreshing", "Coins": 5}, 
+         {"name": "Shovel", "description": "Dig for treasure","Coins": 0}, 
+         {"name": "WW2 Rations", "description": "Food for energy", "Coins": 5},
+         {"name": "Energy Drink", "description": "Drink for Energy", "Coins": 3},
+         {"name": "Fishing Rod", "description": "Fish for Food", "Coins": 6},
+         {"name": "Life Raft", "description": "Escape Boat To FREEDOM", "Coins": 1000}
+        ]
+
+
+
+
+
+while True:
+    for index, items in enumerate(store):
+        print(index, ":", items["name"],"-", "Description:", items["description"], "-", "Cost:", items["Coins"])
+
+    asker = int(input("Buy What?? ")) 
+    if Coins > store[asker]["Coins"]:
+        Coins -= store[asker]["Coins"]
+        cart.append(store[asker]["name"])
+        print(cart)
+        print(Coins)
+    else:
+        print("You're Too Poor")
+
+    asker = input('Do You Want To Continue??').upper()
+    if asker == "NO":
+        break
+
     
     def days(self):
         self.energy -= 10
